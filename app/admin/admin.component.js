dc806101efcd5b52a95fd67814bc9adcf7700cef
Samuel Gil <samuxiii@gmx.com>
2017-03-14 07:10:53 +0100

    Starred items are added to the list when clicked

--- app/admin/admin.component.js
+++ app/admin/admin.component.js
@@ -4,8 +4,8 @@ angular.
   module('admin').
   component('admin', {
     templateUrl: 'admin/admin.template.html',
-    controller: /*['$http',*/
-      function AdminController(/*$http*/) {
+    controller: ['$http',
+      function AdminController($http) {
         var self = this;
 
         self.defaultList = [
@@ -19,13 +19,20 @@ angular.
         /* methods */
         //add a new item to the shopping list
         self.addItem = function(item){
-          /* TODO: add to the shopping list */
-          //remove from the current list
+          $http.post('/api/shoplist', {'text':item})
+                .success(function(data) {
+                    //self.list = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error:' + data);
+                });
+          //remove from the current default list
           var i = self.defaultList.indexOf(item);
           if (i !=-1 ) {
             self.defaultList.splice(i, 1);
           }
         };
       }
-    //]
+    ]
   });
