a5f466e7c6fff38d3708004793f36457ccf9b9de
Samuel Gil <samuxiii@gmx.com>
2017-05-13 01:16:09 +0200

    New test for item-list component

--- app/item-list/item-list.component.spec.js
+++ app/item-list/item-list.component.spec.js
@@ -3,24 +3,33 @@ describe('ItemListControllerTest', function() {
   beforeEach(module('itemList'));
 
   it('executes a dummy test', inject(function() {
-    expect(0).toBe(0);
+    expect(true).toBe(true);
   }));
   
-  var $httpBackend, ctrl;
+  var $httpBackend, ctrl, item;
   
   beforeEach(inject(function($componentController, _$httpBackend_, _$location_) {
+    item = {_id:1, text:"new", star:true, done:true};
     $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/shoplist')
+                .respond([item]);
     $location = _$location_;
-    $ctrl = $componentController('itemList', {$httpBackend, $location});
+    ctrl = $componentController('itemList');
   }));
   
   it('checks certain methods are present', inject(function() {
-    
-    expect($ctrl.createItem).toBeDefined();
-    expect($ctrl.deleteItem).toBeDefined();
-    expect($ctrl.starredItem).toBeDefined();
-    expect($ctrl.updateSelectedItems).toBeDefined();
-    expect($ctrl.wipe).toBeDefined();
+    expect(ctrl.createItem).toBeDefined();
+    expect(ctrl.deleteItem).toBeDefined();
+    expect(ctrl.starredItem).toBeDefined();
+    expect(ctrl.updateSelectedItems).toBeDefined();
+    expect(ctrl.wipe).toBeDefined();
+  }));
+
+  it('should be initialized', inject(function() {
+    expect(ctrl.formItem).toBeDefined();
+    expect(ctrl.user).toBeDefined();
+    $httpBackend.flush();
+    expect(ctrl.list).toEqual([item]);
   }));
 
 });
